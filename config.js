97242fe432af9ec71700b55f93777a4733b92289
fave-eng <fhendytheking@gmail.com>
2026-07-21 19:16:58 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ window.APP_CONFIG = {
   },
 
   supabase: {
-    url: "",
-    anonKey: "",
+    url: "https://zqzgarvmpqqqaobeicpc.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxemdhcnZtcHFxcWFvYmVpY3BjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2ODQwNTIsImV4cCI6MjA5NzI2MDA1Mn0.gARetYwVZfInx3QKS0RvB2I5cOwegPMY5q3nJPX4ZP8",
     tables: {
       homework: "homework_progress",
       vocabulary: "vocabulary_progress",
